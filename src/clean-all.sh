37e834e1b64492d3388b1e18fcbcb3f1f3e75aef
jeromescuggs <subspacer@gmail.com>
2020-08-27 03:45:03 -0500

    tweaking build scripts in /src

--- src/clean-all.sh
+++ src/clean-all.sh
@@ -5,22 +5,15 @@ CUR_DIR=$(dirname $(readlink -f $0))
 
 # Let the person running the script know what's going on.
 echo "Pulling in latest changes for all repositories..."
-sleep 1
-git submodule update --init
 
 # Find all git repositories and update it to the master latest revision
 for i in $(find . -mindepth 1 -maxdepth 1 -type d); do
     # We have to go to the .git parent directory to call the pull command
-    cd "$i"
-	echo "updating ${PWD##*/}"
-	sleep 1
-    git pull origin master
-	sleep 1
-	echo "building ${PWD##*/}"
+    cd "$i" 
+	echo "cleaning ${1##*/}"
 	sleep 1
 	cargo clean 
-	sleep 1
-	cargo build --release;
+	#cargo build --release;
     cd ..;
 
     # finally pull
