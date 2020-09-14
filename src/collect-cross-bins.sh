49835795d1674dec4f2cf4f1b67f3d9374d81cbd
jeromescuggs <subspacer@gmail.com>
2020-09-14 17:14:46 +0000

    script cleanup

--- src/collect-cross-bins.sh
+++ src/collect-cross-bins.sh
@@ -3,9 +3,13 @@
 # store the current dir
 CUR_DIR=$(dirname $(readlink -f $0))
 
+cd $CUR_DIR && mkdir -p ./../bin/aarch64
+
 # Let the person running the script know what's going on.
 echo "collecting compiled binaries from Cargo crate folders..."
 sleep 1
+
+
 
 # Find all git repositories and update it to the master latest revision
 for i in $(find . -mindepth 1 -maxdepth 1 -type d); do
