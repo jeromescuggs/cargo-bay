d7be6d0f6272d3a2a2fde245f1854700924f0b00
jeromescuggs <subspacer@gmail.com>
2020-09-26 01:41:42 -0500

    update git script fixed

--- src/update-all.sh
+++ src/update-all.sh
@@ -5,6 +5,9 @@ CUR_DIR=$(dirname $(readlink -f $0))
 
 # Let the person running the script know what's going on.
 echo "Pulling in latest changes for all repositories..."
+sleep 1s
+
+git submodule update --init
 
 # Find all git repositories and update it to the master latest revision
 for i in $(find . -mindepth 1 -maxdepth 1 -type d); do
