2c71e2c3ce4a8bc7ae33ab0ce5c21022e3732952
jeromescuggs <subspacer@gmail.com>
2020-10-01 03:43:43 +0000

    tweaking the binary collection scripts

---
+++ src/collect-aarch64.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# store the current dir
+CUR_DIR=$(dirname $(readlink -f $0))
+
+cd $CUR_DIR && mkdir -p ./../bin/aarch64
+
+# Let the person running the script know what's going on.
+echo "collecting compiled binaries from Cargo crate folders..."
+sleep 1
+
+
+
+# Find all git repositories and update it to the master latest revision
+for i in $(find . -mindepth 1 -maxdepth 1 -type d); do
+    # We have to go to the .git parent directory to call the pull command
+	cd "$i" 
+	echo "collecting ${PWD##*/}" 
+	cp target/aarch64-unknown-linux-gnu/release/${PWD##*/} ../../bin/aarch64
+    cd ..;
+done
+
+echo "manually moving exceptions..."
+sleep 1
+if [[ -d ./bottom ]]; then
+    echo "moving bottom to bin/armhf/btm"
+    cp $CUR_DIR/bottom/target/aarch64-unknown-linux-gnu/release/btm ../bin/aarch64
+fi 
+
+echo "Complete!"
+sleep 1s
+echo "The binaries have been copied to the 'bin' directory in the root folder."
