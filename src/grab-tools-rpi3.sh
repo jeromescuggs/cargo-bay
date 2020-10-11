e56b45a67f8a0566dd492cad395078e73e3dc4a0
jeromescuggs <subspacer@gmail.com>
2020-10-11 11:34:20 +0000

    updated crates, general reord, more scripts

---
+++ src/grab-tools-rpi3.sh
@@ -0,0 +1,7 @@
+
+
+rustup target add armv7-unknown-linux-gnueabihf
+# git clone https://github.com/raspberrypi/tools $HOME/rpi_tools
+# export RUSTFLAGS="-C linker=$HOME/rpi_tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc" 
+
+# cargo build --target armv7-unknown-linux-gnueabihf 
